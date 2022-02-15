aa87bd204e8f0b494b320d3c35fec8df903fcf91
teeaz <teeaz@github.com>
2022-02-16 01:17:40 +0800

    feat: templates cross correlation support

--- newfile/__main__.py
+++ newfile/__main__.py
@@ -2,6 +2,7 @@ import sys
 import re
 import argparse
 from datetime import datetime
+from inspect import signature
 
 import yaml
 import click
@@ -9,80 +10,159 @@ import click
 import jinja2
 from jinja2 import Environment, FunctionLoader, select_autoescape
 
-
 try:
     from rich.console import Console
     from rich.traceback import install
+    from rich import inspect
     console = Console()
     install(show_locals=True,suppress=[jinja2,click])
+    print = console.print
 except:
     pass
 
-
+template_catch = {}
 def load_template(name):
     if not name:
         return ""
     source = ''
-    with open(f'newfile_templates/{name}.jinja') as f:
-        source = f.read()
-    defs = {}
-    if re.match(r'\s*===+\n',source):
-        s = re.split('\n===+\n',source.lstrip(" =\t\r\n"),1)
-        source = s[1]
-        pre_param_defs(s[0])
+    if name in template_catch:
+        return template_catch[name]['source']
 
-    return source
+    p = f'newfile_templates/{name}.jinja'
+    with open(p) as f:
+        source = f.read()
+
+    data = {
+        'name':name,
+        'path':p,
+        'setup':'{}',
+        'param':{},
+        'source': source
+    }
+    preprocess_template(data)
+    template_catch[name] = data
+    return data['source']
 
 ### init jinja ### 
 tlenv = Environment(
 loader = FunctionLoader(load_template),
 autoescape = select_autoescape(),
 undefined=jinja2.DebugUndefined,
-finalize=lambda s: s not in [None,False] and s or ''
+finalize=lambda s: s not in [None,False] and str(s) or ''
 )
 tlctx = {
-    'params':[],
+    'args':[],
     'opts':{},
+    'state':{},
+    'env':tlenv,
 }
+
+tlenv.globals['_tl_'] = tlctx
+tlenv.globals['args'] = tlctx['args']
+tlenv.globals['opts'] = tlctx['opts']
+tlenv.globals['now'] = datetime.now().ctime()
+
+def tlfilter(func,g=False):
+    name = func.__name__
+    if len(name) > 2 and name[0:2] == 'tl':
+        name = name[2:]
+    tlenv.filters[name] = func
+    if g:
+        tlenv.globals[name] = func
+    return func
+
+def tlfunc(func):
+    return tlfilter(func,True)
+
+
+@tlfunc
+def set_state(value,name):
+    console.print('set_state: ',(name,value))
+    tlctx['state'][name] = value
+    return ''
+
+@tlfilter
+def tlsignature(func):
+    try:
+        return signature(func)
+    except:
+        return str(func)
+
+@tlfilter
+def tlinspect(obj):
+    inspect(obj)
+    return ''
+tlfilter(str)
 # -- init jinja --
 
-def pre_param_defs(text):
-    parser = tlctx['parser']
-    ps = yaml.load(text)
-    for l,r in ps.items():
-        a = []
-        k = argparse.Namespace()
-        l = l.split(',')
-        for i in l:
-            i = i.strip()
-            if len(i) == 0:
-                continue
-            if i[0] == '-':
-                a.append(i)
+def preprocess_template(data): 
+    s = data['source']
+    if re.match(r'\s*===+\n',s):
+        s = re.split('\n===+\n',s.lstrip(" =\t\r\n"),1)
+        data['source'] = s[1]
+        head = {
+            'param':[],
+            'setup':[],
+        }
+        last = 'param'
+        for i in s[0].split('\n'):
+            if i and i[0] == ' ':
+                head[last].append(i)
+            elif i and i[0] == '-':
+                head['param'].append(i)
+                last = 'param'
             else:
-                k.help = i
-        t = type(r)
-        if t in [int,float]:
-            k.type = int
-        elif t == list:
-            k.type = str
-            k.action = 'append'
+                head['setup'].append(i)
+                last = 'setup'
+                
+        data['setup'] = '\n'.join(head['setup'])
+        param = head['param']
+        param = '\n'.join(param)
+        param = inline_render(param)
+        param = yaml.load(param)
+        if type(param) == dict:
+            for l,r in param.items():
+                preprocess_param (l,r,data)
 
-        elif r == False:
-            k.action = 'store_true'
-        elif r == True:
-            k.action = 'store_false'
+
+def preprocess_param(left,right,data):
+    args = []
+    kwargs = argparse.Namespace()
+
+    for i in left.split(','):
+        i = i.strip()
+        if len(i) == 0:
+            continue
+        if i[0] == '-':
+            args.append(i)
         else:
-            k.type = str
+            kwargs.help = i
+    t = type(right)
+    if t in [int,float]:
+        k.type = int
+    elif t == list:
+        kwargs.type = str
+        kwargs.action = 'append'
 
-        k.default = r
-
-
-        try:
-            parser.add_argument(*a,**vars(k))
-        except argparse.ArgumentError as e:
-            pass
+    elif right == False:
+        kwargs.action = 'store_true'
+    elif right == True:
+        kwargs.action = 'store_false'
+    else:
+        kwargs.type = str
     
+    kwargs.default = right
+
+    if kwargs.default == None:
+        kwargs.default = ''
+
+    try:
+        tlctx['parser'].add_argument(*args,**vars(kwargs))
+    except argparse.ArgumentError as e:
+        pass
+
+
+
 
 def post_args(args,opts):
     param = []
@@ -100,63 +180,94 @@ def post_args(args,opts):
     return param
 
 
+
 def pull_args():
     parser :argparse.ArgumentParser = tlctx['parser']
     (opts,args) = parser.parse_known_args(tlctx['argv'])
     opts = vars(opts)
     params = post_args(args,opts)
-    tlctx['params'] = params
+    tlctx['args'] = params
     tlctx['opts'] = opts
-
+    tlenv.globals['args'] = params
     for i in tlctx['opts']:
         s = tlctx['opts'][i]
-        if type(s) == str and '{{' in s:
-            tlctx['opts'][i] = inline_render(s)
         tlenv.globals[i] = tlctx['opts'][i]
 
 
 def inline_render(text):
-    tl = tlenv.from_string(text)
-    return tl.render()
+    if type(text) == str:
+        tl = tlenv.from_string(text)
+        return tl.render()
+    elif type(text) == dict:
+        for i in text:
+            text[i] = inline_render(text[i])
+    elif type(text) == list:
+        for i in range(len(text)):
+            text[i] = inline_render(text[i])
     
+    return text
+
+
+tlargparser = argparse.ArgumentParser(
+    add_help=False
+)
+tlargparser.add_argument('--help','-h','-?',action='store_true', help='display help of template')
+
+def entry(template,args,output=None):
+    tlctx['parser'] = tlargparser
+    tlctx['argv'] = args
+    
+    load_template(template)
+    setup = template_catch[template]['setup']
+
+    @tlfunc
+    def tlsetup(v,k=None):
+        if k:
+            setup[k] = v
+            return ''
+        return setup[k]
+
+    tlhandle = tlenv.get_template(template)
+
+    tlargparser.parse_known_args(args)
+    # pre-process
+    pull_args()
+    tlhandle.render()
+    pull_args()
+
+    setup = inline_render(setup)
+    setup = yaml.load(setup)
+    if type(setup) != dict:
+        setup = {}
+    if tlctx['opts']['help']:
+        tlargparser.print_help()
+        return
+
+    output = output or setup.get('output',template)
+
+    rendered = tlhandle.render(
+        FILENAME=output,
+    )
+
+    mode = setup.get('force',False) and 'x' or 'w'
+    with open(output,mode) as f:
+        f.write(rendered) 
+        print(f'{mode}: {output}')
+
+    for i,o in setup.get('relate',{}).items():
+        entry(i,args,o)
 
 @click.command(context_settings={
 #'ignore_unknown_options':True,
 'allow_interspersed_args':False,
 'allow_extra_args':True
 })
-@click.option('-o','--output',is_flag=True,help='output filename or base dir')
 @click.argument('template')
 @click.argument('args',nargs=-1)
 def main(template,args,**opts):
-    global tlhandle
-    tlenv.globals['_tl_'] = tlctx
-    tlenv.globals['now'] = datetime.now().ctime()
-    tlargparser = argparse.ArgumentParser(
-        prog=template,
-        add_help=False
-    )
-    tlargparser.add_argument('--help','-h','-?',action='store_true', help='display help of template')
-    tlctx['parser'] = tlargparser
-    tlctx['argv'] = args
+    entry(template,args)
     
-    tlhandle = tlenv.get_template(template)
-    tlargparser.parse_known_args(args)
+    
 
-    tlhandle.render()
-    pull_args()
-    if tlctx['opts']['help']:
-        parser.print_help()
-        return
-
-    out = tlhandle.render()
-    
-    output_file = opts['output'] or template
-    with open(output_file,'x') as f:
-        f.write(out)
-    
-    
-    
-    
 if '__main__' == __name__:
     main()
